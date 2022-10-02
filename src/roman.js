9bdab5470d1e0ffe98e70e2efa6db3ec84e8bc10
RosarioRu <mariarruval@gmail.com>
2022-10-02 16:42:36 -0700

    change order of return statements so that the letter is returned BEFORE the remaining amount is sent back into the function.

--- src/roman.js
+++ src/roman.js
@@ -1,6 +1,4 @@
-
-
-export default function recursiveRoman(number) {
+export default function recursiveRoman(number) { 
   if (isNaN(number) ||(number<1) || (number>3999)) {
     return "Not a valid number; please enter a number between 1-3,999";
   }
@@ -8,14 +6,22 @@ export default function recursiveRoman(number) {
     if (number-1000 === 0) {
       return ("M");
     } else {
-    return ((recursiveRoman(number-1000)) + "M");
+    return ("M" + (recursiveRoman(number-1000)));
     }
   }
   if (number>=500) {
     if (number-500 === 0) {
       return ("D");
     } else {
-    return ((recursiveRoman(number-500)) + "D");
+    return ("D" + (recursiveRoman(number-500)));
     }
-  } 
+  }
+  if (number>=100) {
+    if (number-100 === 0) {
+      return ("C");
+    } else {
+    return ("C" + (recursiveRoman(number-100)));
+    }
+  }
+
 };
