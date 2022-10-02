1f65460c14f9fbe53b472b18b3610227b5848067
RosarioRu <mariarruval@gmail.com>
2022-10-02 14:52:17 -0700

    refactor to put two termination edge cases into one single if statement.

--- __tests__/roman.test.js
+++ __tests__/roman.test.js
@@ -5,7 +5,7 @@ describe('recursiveRoman', () => {
   // tests go in here
 
   test("should return an error when the function is given anything other than a real number", () => {
-    expect(recursiveRoman("hi")).toEqual("Not a number.");
+    expect(recursiveRoman("hi")).toEqual("Not a valid number; please enter a number between 1-3,999");
   });
 
   test("should return undefined when a number is passed into it -- for now", () => {
--- src/roman.js
+++ src/roman.js
@@ -1,10 +1,7 @@
 
 
 export default function recursiveRoman(number) {
-  if (isNaN(number)) {
-    return "Not a number.";
-  }
-  if ((number<1) || (number>3999)) {
+  if (isNaN(number) ||(number<1) || (number>3999)) {
     return "Not a valid number; please enter a number between 1-3,999";
   }
   if (number>=1000) {
@@ -14,4 +11,7 @@ export default function recursiveRoman(number) {
     return ((recursiveRoman(number-1000)) + "M");
     }
   }
+  if (number>=500) {
+    //code
+  } 
 };
